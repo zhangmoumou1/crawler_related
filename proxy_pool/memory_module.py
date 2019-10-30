4c44547ce0f4abf12331932c6373d5970b487d30
zhangmoumou1 <932545014@qq.com>
2019-10-30 16:38:43 +0800

    代理池

--- proxy_pool/memory_module.py
+++ proxy_pool/memory_module.py
@@ -1,25 +1,88 @@
 max_score = 100
 min_score = 0
 initial_score = 10
-redis_host = 
-redis_port = 
-redis_password = 
-redis_key = 
+redis_host = 'xxxxxxx'
+redis_port = 6397
+redis_password = 'xxxxxx'
+redis_key = 'proxies'
 
 import redis
 from random import choice
 
 class RedisClient(object):
     def __init__(self, host=redis_host, port=redis_port, password=redis_password):
-        """初始化"""
+        """
+        初始化
+        :return: 登录redis
+        """
         self.db = redis.StrictRedis(host=host, port=port, password=password, decode_responses=True)
 
-    def add(self, proxy, score=INITIAL_SCORE):
+    def add(self, proxy, score=initial_score):
         """
         添加代理，设置分数为最高
         :param proxy: 代理
         :param score: 分数
         :return: 添加结果
         """
-        if not self.db.zscore(REDIS_KEY, proxy):
-            return self.db.zadd(REDIS_KEY, score, proxy)
+        if not self.db.zscore(redis_key, proxy):
+            return self.db.zadd(redis_key, score, proxy)
+
+    def random(self):
+        """
+        随机获取有效代理，首先尝试获取最高分数代理，如果不存在，按照排名获取，否则异常
+        :return: 随机代理
+        """
+        result = self.db.zrangebyscore(redis_key, max_score, max_score)
+        if len(result):
+            return choice(result)
+        else:
+            result = self.db.zrevrange(redis_key, 0, 100)
+            if len(result):
+                return choice(result)
+            else:
+                raise PoolEmptyError
+
+    def decrease(self, proxy):
+        """
+        代理值减一分，小于最小值则删除
+        :param proxy: 代理
+        :return: 修改后的代理分数
+        """
+        score = self.db.zscore(redis_key, proxy)
+        if score and score > min_score:
+            print('代理', proxy, '当前分数', score, '减1' )
+            return self.db.zincrby(redis_key, proxy)
+        else:
+            print('代理', proxy, '当前分数', score, '移除')
+            return self.db.zrem(redis_key, proxy)
+
+    def exists(self, proxy):
+        """
+        判断是否存在
+        :param proxy: 代理
+        :return: 是否存在
+        """
+        return not self.db.zscore(redis_key, proxy) == None
+
+    def max(self, proxy):
+        """
+        将代理设置为 MAX_SCORE
+        :param proxy: 代理
+        :return: 设置结果
+        """
+        print(' 代理 ', proxy, ' 可用，设置为 ', MAX_SCORE)
+        return self.db.zadd(REDIS_KEY, MAX_SCORE, proxy)
+
+    def count(self):
+        """
+        获取数量
+        :return: 数量
+        """
+        return self.db.zcard(REDIS_KEY)
+
+    def all(self):
+        """
+        获取全部代理
+        :return: 全部代理列表
+        """
+        return self.db.zrangebyscore(REDIS_KEY, MIN_SCORE, MAX_SCORE)
---
+++ proxy_pool/obtain_module.py
@@ -0,0 +1,80 @@
+import json
+import requests
+from pyquery import PyQuery as pq
+
+class ProxyMetaclass(type):
+    def __new__(cls, name, bases, attrs):
+        count = 0
+        attrs['__CrawlFunc__'] = []
+        for k, v in attrs.items():
+            if 'crawl_' in k:
+                attrs['__CrawlFunc__'].append(k)
+                count += 1
+        attrs['__CrawlFuncCount__'] = count
+        return type.__new__(cls, name, bases, attrs)
+
+class Crawler(object, metaclass=ProxyMetaclass):
+    def get_proxies(self, callback):
+        proxies = []
+        for proxy in eval("self.{}()".format(callback)):
+            print(' 成功获取到代理 ', proxy)
+            proxies.append(proxy)
+        return proxies
+
+    def crawl_daili66(self, page_count=4):
+        """
+        获取代理 66
+        :param page_count: 页码
+        :return: 代理
+        """
+        start_url = 'http://www.66ip.cn/{}.html'
+        urls = [start_url.format(page) for page in range(1, page_count + 1)]
+        for url in urls:
+            print('Crawling', url)
+            html = requests.get(url).text
+            if html:
+                doc = pq(html)
+                trs = doc('.containerbox table tr:gt(0)').items()
+                for tr in trs:
+                    ip = tr.find('td:nth-child(1)').text()
+                    port = tr.find('td:nth-child(2)').text()
+                    yield ':'.join([ip, port])
+
+    def crawl_kuaidaili(self, page_count=5):
+        """
+        获取代理 快代理
+        : param page_count: 页码
+        : return: 代理
+        """
+        start_url = 'https://www.kuaidaili.com/free/inha/{}/'
+        urls = [start_url.format(page) for page in range(1, page_count + 1)]
+        for url in urls:
+            print('Crawling', url)
+            html = requests.get(url).text
+            if html:
+                doc = pq(html)
+                #:gt(0)是伪类选择器，代表tbpdy下面大于等于1后的tr标签
+                trs = doc('.table.table-bordered.table-striped tbody tr:gt(0)').items()
+                for tr in trs:
+                    ip = tr.find('td:nth-child(1)').text()
+                    port = tr.find('td:nth-child(2)').text()
+                    yield ':'.join([ip, port])
+
+    def crawl_xicidaili(self, page_count=5):
+        """
+        获取代理 西刺
+        : param page_count: 页码
+        : return: 代理
+        """
+        start_url = 'https://www.xicidaili.com/nn/{}'
+        urls = [start_url.format(page) for page in range(1, page_count + 1)]
+        for url in urls:
+            print('Crawling', url)
+            html = requests.get(url).text
+            if html:
+                doc = pq(html)
+                trs = doc('.table.table-bordered.table-striped tbody tr:gt(0)').items()
+                for tr in trs:
+                    ip = tr.find('td:nth-child(1)').text()
+                    port = tr.find('td:nth-child(2)').text()
+                    yield ':'.join([ip, port])
